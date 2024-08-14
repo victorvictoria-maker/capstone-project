517b49e74e02c1d7d3875e66546d9fbd6159ea6d
victorvictoria-maker <victorvictoria0001@gmail.com>
2024-08-14 11:54:36 +0100

    Auth modification 3

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -2,6 +2,7 @@
 // import { createClient } from "@/utils/supabase/server";
 // import { db } from "@/lib/db";
 
+import { db } from "@/lib/db";
 import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
 
@@ -93,9 +94,67 @@ export async function GET(request: Request) {
 
   if (code) {
     const supabase = createClient();
-    await supabase.auth.exchangeCodeForSession(code);
+    // await supabase.auth.exchangeCodeForSession(code);
+
+    try {
+      const { data: session, error } =
+        await supabase.auth.exchangeCodeForSession(code);
+      if (error || !session?.user) {
+        throw new Error(
+          error?.message || "Failed to exchange code for session."
+        );
+      }
+      const { id, user_metadata } = session.user;
+      //   console.log("Session from callback", session.session);
+      //   console.log("User from callback", session.user);
+      const name = user_metadata?.full_name;
+      const email = user_metadata?.email;
+      const image = user_metadata?.picture;
+      //   check if user exits with that email
+      const existingUser = await db.user.findUnique({
+        where: { email },
+      });
+      if (existingUser) {
+        // check if it is a first time user
+        if (existingUser.createdAt) {
+          return NextResponse.redirect(
+            `${process.env.NEXT_PUBLIC_APP_URL}${next}`
+          );
+        } else {
+          return NextResponse.redirect(
+            `${process.env.NEXT_PUBLIC_APP_URL}/auth?error=There is a user with this email!`
+          );
+        }
+      } else {
+        // Create a new user in db
+        await db.user.create({
+          data: {
+            id: id,
+            email: email,
+            name: name,
+            image,
+            role: "USER",
+          },
+        });
+        // return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}/auth?success=AccountCreated`);
+        // Redirect to the desired page after successful login
+        console.log("Auth callback sucessffuyl login");
+        return NextResponse.redirect(
+          `${process.env.NEXT_PUBLIC_APP_URL}${next}`
+        );
+      }
+    } catch (err) {
+      console.error("OAuth Callback Error:", err);
+      return NextResponse.redirect(
+        `${process.env.NEXT_PUBLIC_APP_URL}/auth/auth-code-error`
+      );
+    }
   }
 
   // return NextResponse.redirect(requestUrl.origin);
-  return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}${next}`);
+
+  // return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}${next}`);
+  return NextResponse.redirect(
+    `${process.env.NEXT_PUBLIC_APP_URL}/auth/auth-code-error`
+  );
 }
